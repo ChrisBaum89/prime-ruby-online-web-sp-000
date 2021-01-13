638033426653d42099c0e8bcdd75368d26c015fa
Chris Baum <christopher.baum@valpo.edu>
2021-01-13 01:56:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,5 @@
 # Add  code here!
-number = 7
+#number = 7
 def prime?(number)
   last_divider_num = number - 1
   number_array = Array (2..last_divider_num)
@@ -15,4 +15,4 @@ def prime?(number)
   end
   true
 end
-puts "#{prime?(number)}"
+#puts "#{prime?(number)}"
