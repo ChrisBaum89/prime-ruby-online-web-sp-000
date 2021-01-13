04f61450cdc39f159e0bcef159843c496092ed2d
Chris Baum <christopher.baum@valpo.edu>
2021-01-13 01:56:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -9,7 +9,7 @@ def prime?(number)
     num_divided = number/i
     #puts "#{num_divided}"
     #puts "#{(num_divided % 1).zero?}"
-    if (num_divided % 1).zero? == true
+    if (num_divided % 1).zero? == true || number < 2
       return false
     end
   end
