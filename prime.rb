1a8c7bf18427e43bf68f0665239330003f2ee1be
Chris Baum <christopher.baum@valpo.edu>
2021-01-13 01:48:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,5 +3,9 @@ number = 7
 def prime?(number)
   last_divider_num = number - 1
   number_array = Array (2..last_divider_num)
+  number_array.each do |i|
+    num_divided = number/i
+    (num_divided % 1).zero?
+  end
 end
 puts "#{prime?(number)}"
